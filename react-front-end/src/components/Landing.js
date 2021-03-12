21981c1bc044e774c3eb4e22d10cc41f75e1e76f
gforsythe <gabe.forsythe@gmail.com>
2021-03-12 23:45:52 +0000

    begun work on the frontend

---
+++ react-front-end/src/components/Landing.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from '@material-ui/core/button';
+
+
+
+function Landing() {
+  return (
+
+    <div>
+      <Button variant="contained" color="primary"> Log In</Button>
+
+
+      <Button variant="contained" color="primary">SignUp</Button>
+    </div>
+
+  );
+}
+
+
+
+
+
+
+export default Landing;
---
+++ react-front-end/src/components/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme} from '@material-ui/core/styles'
+import {orange} from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[300],
+    }
+  }
+})
+
+
+export default theme;
