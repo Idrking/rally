f5fc62a8fa95e18edf884a96ad5b1bb107673194
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-11 19:21:45 -0800

    Table update

--- express-back-end/db/schema/04_tasks.sql
+++ express-back-end/db/schema/04_tasks.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS tasks CASCADE;
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY NOT NULL,
-  organization_id INTEGER REFERENCES organizations(id) ON DELETE CASCADE,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE,
   spots INTEGER,
   image_url VARCHAR(255) NOT NULL,
+  organization_id INTEGER REFERENCES organizations(id) ON DELETE CASCADE,
   location VARCHAR(255)
 );
 ALTER TABLE tasks OWNER TO labber;
