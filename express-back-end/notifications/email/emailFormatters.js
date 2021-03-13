7dabf39b37824fa8a5d66b56816a6ceca0a19f08
Isaac King <idrkings@gmail.com>
2021-03-13 21:03:14 +0000

    Add complete email functionality on task creation

---
+++ express-back-end/notifications/email/emailFormatters.js
@@ -0,0 +1,41 @@
+
+const formatText = details => {
+  return `
+  Hello volunteer!
+  A new task has been issued by ${details.organization}:
+
+  ${details.name}
+  ${details.start_date}
+
+  If you're available, or need more details, please visit
+  http://rally.com/tasks/${details.id}
+  `
+}
+
+const formatHTML = details => {
+  return `
+  <h3>New Task</h3>
+  <p> Hello volunteer! </p>
+  <p> A new task has been issued by <strong>${details.organization}</strong></p>
+  <hr>
+  <h3>${details.name}</h3>
+  <h4>${details.start_date}</h4>
+  <hr>
+  <p> If you're available, or need more details, please click below
+  <br>
+  <a href="http://rally.com/tasks/${details.id}"><button>Signup</button></a>
+  `
+}
+
+
+const formatEmailObject = details => {
+  return {
+    from: '"Rally" <notifications@rally.com>',
+    to: "sally@examplevolunteer.com",
+    subject: `New Task from ${details.organization}`,
+    text: formatText(details),
+    html: formatHTML(details)
+  };
+}
+
+module.exports = { formatEmailObject };
--- express-back-end/notifications/email/sendEmail.js
+++ express-back-end/notifications/email/sendEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const emailTask = async(taskDetails) => {
+const emailTask = async(details) => {
   
   //For demonstration purposes, we create an Ethereal Account
   //Should this actually be produced, we'd need to swap this out with a proper SMTP client to send the emails
@@ -15,17 +15,11 @@ const emailTask = async(taskDetails) => {
       pass: testAccount.pass
     }
   });
-  const emailDetails = {
-    from: '"Rally" <noreply@rally.com>',
-    to: "test@email.com",
-    subject: "New Task from Wendy",
-    text: "Lorgy Borgy",
-    html: `<h1>Lorgy Porgy</h1>`
-  }
-  const taskEmail = await transporter.sendMail(emailDetails)
 
-  console.log("Message sent: %s", taskEmail.messageId);
+  const taskEmail = await transporter.sendMail(details)
+  
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(taskEmail));
+  console.log("Message sent: %s", taskEmail.messageId);
 }
 
 module.exports = { emailTask };
