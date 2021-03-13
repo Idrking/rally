326d359701ff83fa6d91cc3334d1295f50957ad3
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-13 15:05:07 -0800

    Spacing

--- react-front-end/src/components/Tasks.js
+++ react-front-end/src/components/Tasks.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { Card, CardActionArea, CardContent, Typography } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(32),
+      height: theme.spacing(16),
+    }
+  },
+  customBorderRadius: {
+    borderRadius: 25
+  }
+}));
+
 function Tasks() {
+  const classes = useStyles();
   return (
-    <Card elevation={10}>
+    <Card elevation={10} className={classes.root}>
       <CardActionArea>
         <CardContent>
           <Typography variant="h2">Task</Typography>
