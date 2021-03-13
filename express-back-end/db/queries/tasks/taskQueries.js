86393911683fb88b2f260e801c7196e62c874ee6
Isaac King <idrkings@gmail.com>
2021-03-13 01:40:05 +0000

    Finish routes for tasks

--- express-back-end/db/queries/tasks/taskQueries.js
+++ express-back-end/db/queries/tasks/taskQueries.js
@@ -6,7 +6,7 @@ module.exports = {
   `,
 
   //02_specific.sql
-  specificTasks:`
+  specificTask:`
     SELECT *
     FROM tasks
     WHERE id = $1;
@@ -26,5 +26,20 @@ module.exports = {
     SELECT COUNT(*)
     FROM signups
     WHERE task_id = $1;
+  `,
+
+  //05_new_task.sql
+  newTask:`
+    INSERT INTO tasks (name, description, start_date, end_date, spots, image_url, organization_id, location)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
+  `,
+
+  //06_edit_task.sql
+  editTask:``,
+
+  //07_delete_task.sql
+  deleteTask:`
+    DELETE FROM tasks
+    WHERE id = $1;
   `
 };
--- express-back-end/routes/tasks.js
+++ express-back-end/routes/tasks.js
@@ -2,6 +2,7 @@ const express           = require("express");
 const router            = express.Router();
 const { deliverError }  = require("./helpers/routeHelpers");
 const taskQueries       = require("../db/queries/tasks/taskQueries");
+const { response } = require("express");
 
 
 module.exports = (db) => {
@@ -24,33 +25,55 @@ module.exports = (db) => {
 
   // Gets a specific task
   router.get("/:id", (req, res) => {
-
+    db.query(taskQueries.specificTask, [req.params.id])
+    .then(tasks => res.json(tasks.rows))
+    .catch(err => res.status(500).send(deliverError(err.message)));
   });
 
   // Gets all tasks
   router.get("/", (req, res) => {
-    
+    db.query(taskQueries.allTasks)
+    .then(tasks => res.json(tasks.rows))
+    .catch(err => res.status(500).send(deliverError(err.message)));
   });
 
   // PUT ROUTES ---------------------------------------------
 
   // Adds a new task
   router.put("/", (req, res) => {
-
+    const queryParams = [
+      req.body.name,
+      req.body.description,
+      req.body.start_date,
+      req.body.end_date,
+      req.body.spots,
+      req.body.image_url,
+      req.body.organization_id,
+      req.body.location
+    ];
+    
+    db.query(taskQueries.newTask, queryParams)
+    .then(() => {
+      // send a text message here
+      res.status(201)
+    })
+    .catch(err => res.status(500).send(deliverError(err.message)));
   });
 
   // PATCH ROUTES ---------------------------------------------
 
   // Edits a task
   router.patch("/:id", (req, res) => {
-
+    //TODO
   });
 
   // DELETE ROUTES ---------------------------------------------
   
   // Deletes a task
   router.delete("/:id", (req, res) => {
-
+    db.query(taskQueries.deleteTask, [req.params.id])
+    .then(() => res.status(200))
+    .catch(err => res.status(500).send(deliverError(err.message)))
   });
   
   return router;
