aa5c7f2b3ed5233e4851847d782e739c7fa02a45
Isaac King <idrkings@gmail.com>
2021-03-14 00:43:57 +0000

    Moved tasks to appropriate router

--- react-front-end/src/components/routers/TaskRouter.js
+++ react-front-end/src/components/routers/TaskRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Header from '../Header';
+import Tasks from '../Tasks';
 
 const TasksRouter = () => {
   return (
@@ -8,7 +9,7 @@ const TasksRouter = () => {
         <Route path="/tasks/:id">
           <>
             <Header />
-            Render a specific task
+            <Tasks />
           </>
         </Route>
         <Route path="/tasks">
