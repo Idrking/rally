8b3adcfe685227a76af073d81c274ae263313f98
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-14 12:52:36 -0700

    Style card, center it

--- react-front-end/src/components/Organizations.js
+++ react-front-end/src/components/Organizations.js
@@ -1,24 +1,56 @@
 import React from "react";
-import { Card, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Card,
+} from "@material-ui/core";
 
-function Orgnizations() {
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 400,
+    margin: "auto",
+    marginTop: 30,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function Organizations() {
+  const classes = useStyles();
 
   return (
-    <Grid container direction="column">
-      <br/>
-      <br/>
-      <Grid item container>
-        {/* Gutter left side */}
-        <Grid xs={0} sm={2}/>
-        <Grid item xs={12} sm={10}>
-          <Card>11</Card>
-        </Grid>
-        {/* Gutter right side */}
-        <Grid xs={0} sm={2}/>
-      </Grid>
-
-    </Grid>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://pnwraptors.com/wp-content/uploads/2020/05/105-18-048-e1589565999168.jpg"
+          title="Raptor"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            The Raptors
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Wildlife organization dedicated to the conservation of birds of
+            prey. Visitor Centre in Duncan, BC. Bird abatement/wildlife
+            management across Canada.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="medium" color="primary">
+          Join
+        </Button>
+        <Button size="medium" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
-
-export default Orgnizations;
