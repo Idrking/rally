394be457a0d67b81b4899ce079c1fe71fe27e82c
Isaac King <idrkings@gmail.com>
2021-03-15 03:30:50 +0000

    Begin creating authenticate functions

--- express-back-end/routes/helpers/authenticate.js
+++ express-back-end/routes/helpers/authenticate.js
@@ -1,4 +1,4 @@
-
+const orgQueries = require("../../db/queries/organizations/organizationQueries");
 
 module.exports = {
   //Checks if request is made by a logged-in user
@@ -6,8 +6,33 @@ module.exports = {
     return session.userID;
   },
 
-  //
-  owner: function(session, db) {
+  // checks to see if the logged-in user owns the organization
+  organizationOwner: function(session, orgId, db) {
+    return db.query(orgQueries.allOwners, [orgId])
+    .then(owners => {
+      for (const owner of owners.rows) {
+        if (session.userID === owner.id) {
+          return true;
+        }
+        return false; 
+      }
+    })
+  },
+
+  //Checks to see if the currently logged in user can sign up for this task
+  canSignUp: function(session, params, db) {
+    if (!session.userID === params.userid) {
+      return Promise.resolve(false);
+    }
+
+    return db.query(`
+      SELECT approved_users.id
+      FROM approved_users
+      JOIN organizations ON organizations.id = approved_users.organization_id
+      JOIN tasks ON organizations.id = tasks.organization_id
+    
+    `)
+
 
   }
   
