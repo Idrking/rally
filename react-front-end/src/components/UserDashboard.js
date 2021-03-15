9e0d7db7302b9e27f3351dcd74f1f12c14948301
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-14 17:35:44 -0700

    Initial user dashboard component

---
+++ react-front-end/src/components/UserDashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Tasks from "./Tasks"
+import { Typography } from "@material-ui/core";
+
+export default function Organizations() {
+
+  return (
+
+    <div>
+    <Typography>Available tasks</Typography>
+    <Tasks></Tasks>
+    </div>
+
+  );
+}
