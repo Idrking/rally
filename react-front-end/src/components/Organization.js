9ca26c0dea542044ed5849c5b3706ed3352b0210
Isaac King <idrkings@gmail.com>
2021-03-15 14:50:32 -0700

    Add initial modal for task creation

---
+++ react-front-end/src/components/Organization.js
@@ -0,0 +1,21 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+import SimpleBottomNavigation from "./OrganizationNav";
+
+export default function Organization() {
+
+
+
+  return (
+  <Container>
+    <Container maxWidth="sm">
+      Dogs
+    </Container>
+    <Container maxWidth="sm">
+      Cats  
+    </Container>    
+    
+    <SimpleBottomNavigation />
+  </Container>
+  );
+};
---
+++ react-front-end/src/components/TaskCreateForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TaskCreateForm = () => {
+
+}
+
+export default TaskCreateForm;
