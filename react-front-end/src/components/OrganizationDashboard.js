c7b61225896102b024d98fd4961a0bdac83fbb41
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-15 13:18:34 -0700

    Create org dashboard page

--- react-front-end/src/components/OrganizationDashboard.js
+++ react-front-end/src/components/OrganizationDashboard.js
@@ -3,22 +3,32 @@ import Tasks from "./Tasks";
 import { Typography, Button } from "@material-ui/core";
 
 export default function OrganizationDashboard() {
-
   return (
     <div>
-      <br/>
-      <Typography variant="h4" component="h2">Available tasks</Typography>
-      <Tasks></Tasks>
-      <br/>
-      <Button type="submit"
-        variant="contained"
-        color="primary">Create New Task</Button>
-      {" "}
-      <Button type="submit"
-        variant="contained"
-        color="primary">Manage Volunteers</Button>
+      <div>
+        <br />
+        <Typography variant="h5" component="h2">
+          Organization Dashboard
+        </Typography>
+        <br />
+        <Button type="submit" variant="contained" color="primary">
+          Create New Task
+        </Button>
+        <Tasks />
+      </div>
+      <div>
+      <br />
+        <Button type="submit" variant="contained" color="primary">
+          Tasks
+        </Button>
+      </div>
+
+      <div>
+      <br />
+        <Button type="submit" variant="contained" color="primary">
+          Manage Volunteers
+        </Button>
+      </div>
     </div>
-
-
   );
 }
