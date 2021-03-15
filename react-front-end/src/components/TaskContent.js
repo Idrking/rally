bbedcb0390703951f06303cc3be770511dcc94e4
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-15 10:22:17 -0700

    Fix task cards:

--- react-front-end/src/components/TaskContent.js
+++ react-front-end/src/components/TaskContent.js
@@ -1,16 +1,18 @@
-import { Grid } from '@material-ui/core';
-import React from 'react';
-import TaskCard from './TaskCard';
+import { Grid } from "@material-ui/core";
+import React from "react";
+import TaskCard from "./TaskCard";
 
 export default function TaskContent() {
   return (
-    <Grid item container spacing={4}>
-      <Grid item xs={12} sm={10}>
+    <div>
+      <div>
+        {" "}
         <TaskCard />
-      </Grid>
-      <Grid item xs={12} sm={10}>
+      </div>
+      <div>
+        {" "}
         <TaskCard />
-      </Grid>
-    </Grid>
+      </div>
+    </div>
   );
 }
