8e8e07451d53176fd31f224b33ce01d1896c22a5
Isaac King <idrkings@gmail.com>
2021-03-17 05:02:02 +0000

    Removed unneeded console log

--- react-front-end/src/components/Tasks/TaskContent.js
+++ react-front-end/src/components/Tasks/TaskContent.js
@@ -6,17 +6,11 @@ export default function TaskContent({ tasks }) {
     <div>
 
       {tasks.map(task => {
-        console.log('this is task id', task.id)
-        console.log('this is task', task)
         return (
-<<<<<<< HEAD
-            <TaskCard key={task.id} task={task}/>
-=======
           <div key={task.id}>
             <TaskCard key={task.id}   task={task}/>
             <br />
           </div>
->>>>>>> master
         );
       })}
     </div>
