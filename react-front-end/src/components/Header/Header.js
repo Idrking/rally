61875e66b3e29fc2a51434cc8d307ab2644e9054
Isaac King <idrkings@gmail.com>
2021-03-18 22:29:19 +0000

    Added dashboard link

--- react-front-end/src/components/Header/Header.js
+++ react-front-end/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import UserContext from "../../contexts/UserContext";
 import MyOrganizations from "./MyOrganizations";
+import { Link } from "react-router-dom";
 
 
 export default function Header() {
@@ -12,6 +13,9 @@ export default function Header() {
       <Typography>
         Rally header
       </Typography>
+      {userState.id && <Link to={`/users/${userState.id}`} >
+        Dashboard
+        </Link>}
       {userState.id && <MyOrganizations />}
     </Toolbar>
   </AppBar>
