7a144596abc2d3509be7806dd6500f3c8ebefe59
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-20 11:53:26 -0700

    Restyle user menu

--- react-front-end/src/components/Header/HeaderUser.js
+++ react-front-end/src/components/Header/HeaderUser.js
@@ -1,26 +1,92 @@
 import React, { useContext } from "react";
-import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  MenuItem,
+  Menu,
+  makeStyles,
+} from "@material-ui/core";
 import UserContext from "../../contexts/UserContext";
 import "../../styles/HeaderUser.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export default function Header() {
   const { userState } = useContext(UserContext);
+
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div>
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        style={{ background: "#F5F5F5", boxShadow: "none", padding: 15}}
+      >
         <Toolbar>
-          <div>
-            <span>Hi {userState.id && <p> {userState.name}</p>}</span>
-          </div>
+          <IconButton edge="start" aria-label="menu" >
+            <MenuIcon style={{ color: "#94A8A3", fontSize: 35 }} />
+          </IconButton>
 
-          <Typography>
-            <span role="img" aria-label="megaphone">
-              📣{" "}
-            </span>
-            Rally!
-          </Typography>
-
-          <Button color="inherit">My Organizations</Button>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+              >
+                <AccountCircle style={{ color: "#94A8A3", fontSize: 35 }} />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>My Organizations</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </div>
