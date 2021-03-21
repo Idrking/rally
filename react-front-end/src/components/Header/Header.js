191fd61a48f4db78898f175c6c56043a89974cf0
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-21 13:43:27 -0700

    Change the header

--- react-front-end/src/components/Header/Header.js
+++ react-front-end/src/components/Header/Header.js
@@ -1,22 +1,33 @@
 import React, { useContext } from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, IconButton, Avatar } from "@material-ui/core";
 import UserContext from "../../contexts/UserContext";
 import MyOrganizations from "./MyOrganizations";
 import { Link } from "react-router-dom";
-
+import SortIcon from "@material-ui/icons/Sort";
 
 export default function Header() {
   const { userState } = useContext(UserContext);
-  return <AppBar position="static">
-    <Toolbar>
-      { userState.id && <p>{userState.name}</p>}
-      <Typography>
-        Rally header
-      </Typography>
-      {userState.id && <Link to={`/users/${userState.id}`} >
-        Dashboard
-        </Link>}
-      {userState.id && <MyOrganizations />}
-    </Toolbar>
-  </AppBar>
+  return (
+    <AppBar
+      position="static"
+      style={{
+        backgroundColor: "transparent",
+        color: "#4B6253",
+        boxShadow: "none",
+        padding: 15,
+      }}
+    >
+      <Toolbar>
+        {/* {userState.id && <p>{userState.name}</p>} */}
+        {userState.id && <MyOrganizations />}
+        {userState.id && <Link to={`/users/${userState.id}`}>        <Avatar
+          src="https://i.ibb.co/2ssp8r7/2.jpg"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          style={{ border: "3px solid #B6C7C3" }}
+        /></Link>}
+      </Toolbar>
+    </AppBar>
+  );
 }
--- react-front-end/src/components/Header/MyOrganizations.js
+++ react-front-end/src/components/Header/MyOrganizations.js
@@ -1,10 +1,11 @@
-import React, {useState, useContext, useEffect} from 'react';
-import Axios from 'axios';
-import UserContext from '../../contexts/UserContext';
-import { Button, Menu, MenuItem, Grow} from '@material-ui/core';
+import React, { useState, useContext, useEffect } from "react";
+import Axios from "axios";
+import UserContext from "../../contexts/UserContext";
+import { Button, Menu, MenuItem, Grow, IconButton } from "@material-ui/core";
 import CompactOrgListItem from "./CompactOrgListItem";
-import AddOrgButton from './AddOrgButton';
-import NewOrgForm from './NewOrgForm';
+import AddOrgButton from "./AddOrgButton";
+import NewOrgForm from "./NewOrgForm";
+import SortIcon from "@material-ui/icons/Sort";
 
 export default function MyOrganizations() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -12,13 +13,12 @@ export default function MyOrganizations() {
   const { userState } = useContext(UserContext);
 
   useEffect(() => {
-
     if (userState.id) {
       Axios.get(`/api/users/${userState.id}/organizations/owns`)
-      .then(res => setOrgs(res.data))
-      .catch(err => console.error(err));
+        .then((res) => setOrgs(res.data))
+        .catch((err) => console.error(err));
     }
-  }, [userState])
+  }, [userState]);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -30,9 +30,15 @@ export default function MyOrganizations() {
 
   return (
     <div>
-      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        My Organizations
-      </Button>
+      <IconButton
+        edge="start"
+        aria-label="menu"
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <SortIcon style={{ color: "#6D7E73", fontSize: 35 }} />
+      </IconButton>
       <Menu
         id="my-organizations"
         anchorEl={anchorEl}
@@ -41,16 +47,16 @@ export default function MyOrganizations() {
         onClose={handleClose}
         TransitionComponent={Grow}
       >
-      {orgs.map(org => {
-        return (
-          <MenuItem key={org.id} onClick={handleClose}>
-            <CompactOrgListItem org={org} />
-          </MenuItem>
-        );
-      })}
-      <MenuItem key="add">
-        <AddOrgButton FormComponent={NewOrgForm} />
-      </MenuItem>
+        {orgs.map((org) => {
+          return (
+            <MenuItem key={org.id} onClick={handleClose}>
+              <CompactOrgListItem org={org} />
+            </MenuItem>
+          );
+        })}
+        <MenuItem key="add">
+          <AddOrgButton FormComponent={NewOrgForm} />
+        </MenuItem>
       </Menu>
     </div>
   );
