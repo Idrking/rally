71fc12421438e88f8cf2782026798c3fc9feb56d
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-23 14:45:21 -0700

    Restyle manage and pending volunteer headers

--- react-front-end/src/components/Volunteers/PendingVolunteers.js
+++ react-front-end/src/components/Volunteers/PendingVolunteers.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import VolunteerCard from "./VolunteerCard";
-import { Button, Typography } from "@material-ui/core";
+import { AppBar, Button, Toolbar, Typography } from "@material-ui/core";
 import axios from "axios";
 import { useParams, Link } from "react-router-dom";
 import VolunteerContext from "../../contexts/VolunteerContext";
+import { ArrowBackIos } from "@material-ui/icons";
 
 export default function PendingVolunteers() {
   const { id } = useParams();
@@ -22,27 +23,28 @@ export default function PendingVolunteers() {
 
   return (
     <div className={"backgrounduser"}>
+      <AppBar
+        position="sticky"
+        style={{
+          color: "#6D7E73",
+          backgroundColor: "transparent",
+          boxShadow: "none",
+          padding: 15
+        }}
+      >
+        <Toolbar>
+          <Link to={`/organizations/${id}/manage_volunteers`}>
+            <ArrowBackIos />
+          </Link>
+        </Toolbar>
+      </AppBar>
       <VolunteerContext.Provider
         value={{ pendingVolunteers, setPendingVolunteers }}
       >
         <section className={"dashboardsection"}>
-          <Typography variant="h2" component="h2">
+          <Typography variant="h2" component="h2" gutterBottom style={{paddingBottom: 25}}>
             <b>Pending</b> Volunteers
           </Typography>
-          <br />
-          <div>
-            <Link to={`/organizations/${id}/manage_volunteers`}>
-              <Button type="submit" variant="contained" color="primary">
-                Manage Volunteers
-              </Button>
-            </Link>{" "}
-            <Link to={`/organizations/${id}/dashboard`}>
-              <Button size="medium" color="primary" variant="contained">
-                Back to Dashboard
-              </Button>
-            </Link>
-          </div>
-          <br />
 
           {pendingVolunteers.map((volunteer) => {
             return (
